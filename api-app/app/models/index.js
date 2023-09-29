beba541786538ac7c47bb55c134d0dd3a8c91963
bagjaalam1 <bagjaalam1@gmail.com>
2023-09-29 20:16:24 +0700

    hapus folder yang salah

--- api-app/app/models/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const dbConfig = require('../../config/db.config')
-
-const db = {}
-db.mongoose = mongoose
-db.url = dbConfig.url
-db.users = require('./users.model')
-
-module.exports = db
